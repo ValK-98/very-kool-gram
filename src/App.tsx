1c6946b59b62de6206c58be7a3a7d1a39bf937f2
ValK-98 <vkornijenka@gmail.com>
2024-01-25 14:39:20 +1000

    main page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import './globals.css';
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import { Home } from './_root/pages';
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/forms/AuthLayout";
+import RootLayout from "./_root/pages/RootLayout";
+import { Home } from "./_root/pages";
 
 const app = () => {
   return (
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
       <Routes>
         {/* Public Routes */}
-        <Route path="/sign-in" element={< SigninForm />} />
-        <Route path="/sign-up" element={< SignupForm />} />
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-        
         {/* Private Routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
-
     </main>
-  )
-}
+  );
+};
 
-export default app
+export default app;
