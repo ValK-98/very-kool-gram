b301cc4e4eb5fd40680e53b29bec43409c4d4d45
ValK-98 <vkornijenka@gmail.com>
2024-01-25 13:56:11 +1000

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+
+import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
 
 const app = () => {
   return (
-    <div>app</div>
+    <main className='flex h-screen'>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/sign-in" element={< SigninForm />} />
+        <Route path="/sign-up" element={< SignupForm />} />
+
+        
+        {/* Private Routes */}
+        <Route index element={<Home />} />
+      </Routes>
+
+    </main>
   )
 }
 
